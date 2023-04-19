ac029a74f6ed2d2b78c1fa8b488976603462a318
王骞 <wq19990222>
2023-04-19 14:11:51 +0800

    管理员端提交代码

---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  // BASE_API: '"192.168.100.130:9001"',
+  BASE_API: '"http://localhost:9001"',
+}
---
+++ src/api/edu/video.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+export default {
+
+  //添加小节
+  addVideo(video) {
+    return request({
+      url: '/eduservice/video/addVideo',
+      method: 'post',
+      data: video
+    })
+  },
+
+  //删除小节
+  deleteVideo(id) {
+    return request({
+      url: '/eduservice/video/' + id,
+      method: 'delete'
+    })
+  },
+  //删除视频
+  deleteAliyunvod(id) {
+    return request({
+      url: '/eduvod/video/removeAlyVideo/' + id,
+      method: 'delete'
+    })
+  },
+  //获取小节
+  getVideo(id) {
+      return request({
+        url : '/eduservice/video/' + id,
+        method: 'get'
+      })
+  },
+  // 修改小节
+  updateVideo(video) {
+    return request({
+      url: '/eduservice/video/updateVideo',
+      method: 'put',
+      data: video
+    })
+  },
+  //修改小节
+  // updateVideo(video) {
+  //   return request({
+  //     url: '/eduservice/video/updateVideo',
+  //     method: 'put',
+  //     data: video
+  //   })
+  // },
+
+
+}
